1dabffde9ddbe4460b205d93b4f19ed9014541a4
Waldyr Felix <waldyrfelix@gmail.com>
2021-03-17 12:18:35 -0300

    Adicionado .env

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/gorilla/mux v1.8.1-0.20200912192056-d07530f46e1e
 	github.com/jinzhu/gorm v1.9.17-0.20200921022817-466b344ff592
 	github.com/jinzhu/inflection v1.0.1-0.20210111022912-b5281034e75e // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/waldyrfelix/mockapi/app"
 )
 
 func main() {
+	godotenv.Load()
+
 	fmt.Print("Initializing server...")
+
 	app := &app.App{}
 	app.SetRouters()
 	app.SetMiddlewares()
 
-	fmt.Println("Server on localhost:3000.")
+	fmt.Println("Server on localhost:" + os.Getenv("PORT"))
 
 	app.Run(":" + os.Getenv("PORT"))
 }
